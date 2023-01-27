fa082e19df6931740c30cebd58c778c83894c0ce
riccardofano <rcr.fano@gmail.com>
2023-01-27 13:59:21 +0100

    feat: add reply form

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,7 +1,8 @@
-import { Component, For, Show } from "solid-js";
+import { Component, createSignal, For, Show } from "solid-js";
 import { Comment as CommentType } from "../types";
 
 import "./Comment.scss";
+import "./form/Form.scss";
 
 interface CommentProps {
   comment: CommentType;
@@ -9,6 +10,8 @@ interface CommentProps {
 }
 
 const Comment: Component<CommentProps> = (props) => {
+  const [isReplying, setIsReplying] = createSignal(false);
+
   return (
     <li class="comment" classList={{ [props?.class]: props.class?.length > 0 }}>
       <div class="comment__box">
@@ -21,13 +24,30 @@ const Comment: Component<CommentProps> = (props) => {
           <span class="comment__name">{props.comment.user.name}</span>
           <span class="comment__username">{props.comment.user.username}</span>
         </p>
-        <button class="comment__reply-btn">Reply</button>
+        <button
+          class="comment__reply-btn"
+          onClick={() => setIsReplying(!isReplying())}
+        >
+          Reply
+        </button>
         <p class="comment__content">
           <Show when={props.comment.replyingTo}>
             <span class="comment__mention">@{props.comment.replyingTo}</span>
           </Show>
           {props.comment.content}
         </p>
+        <Show when={isReplying()}>
+          <form class="reply-form">
+            <textarea
+              class="form__textarea"
+              name="reply"
+              placeholder="Type your reply here"
+            />
+            <button class="btn btn--purple" type="submit">
+              Post reply
+            </button>
+          </form>
+        </Show>
       </div>
 
       <Show when={props.comment.replies}>
