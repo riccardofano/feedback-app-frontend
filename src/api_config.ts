54c39d211b3dcedb025922d29421e1649ccb2286
riccardofano <rcr.fano@gmail.com>
2023-01-30 11:34:03 +0100

    build: only use prod server in prod

--- src/api_config.ts
+++ src/api_config.ts
@@ -1,6 +1,8 @@
 import default_axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:8000";
 export const axios = default_axios.create({
   baseURL: API_URL,
 });
