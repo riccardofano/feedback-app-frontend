f459c0646be0b975633a478b8ac0743de2598421
Riccardo Fano <rcr.fano@gmail.com>
2023-07-02 13:42:22 +0200

    feat: notice for posts not appearing immediately

--- src/components/NoSuggestions.tsx
+++ src/components/NoSuggestions.tsx
@@ -6,11 +6,11 @@ import "./NoSuggestions.scss";
 const NoSuggestions: Component = () => {
   return (
     <div class="no-suggestions">
-      <img
-        style={{ width: "23px", height: "24px" }}
-        src="/assets/suggestions/illustration-empty.svg"
-        alt=""
-      />
+      <img src="/assets/suggestions/illustration-empty.svg" alt="" />
+      <p>If this is your first time loading the page, please hang on.</p>
+      <p>
+        The backend is spinning up, I'm using the free tier for this demo :(
+      </p>
       <h2>There is no feedback yet.</h2>
       <p>Got a suggestion? Found a bug that needs to be squashed?</p>
       <p>We love hearing about new ideas to improve our app.</p>
